ff3e7dd433017591f102681901317ea9ac31ecaa
Priscila <priscilaresantos@gmail.com>
2021-07-09 13:25:44 -0300

    Versionando as alterações no stashes e recuperando com o apply no master

--- DevCars.API/Program.cs
+++ DevCars.API/Program.cs
@@ -8,6 +8,7 @@ namespace DevCars.API
         public static void Main(string[] args)
         {
             CreateHostBuilder(args).Build().Run();
+            //Teste realizado para o recurso de stash git
         }
 
         public static IHostBuilder CreateHostBuilder(string[] args) =>
